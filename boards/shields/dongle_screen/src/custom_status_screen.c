6b7feb098728efea6edc0d20e924b4ced64cdf5c
TooManyLs <toomanyls.2hi@gmail.com>
2026-02-07 02:27:54 +0700

    move wpm counter

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -66,7 +66,7 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_WPM_ACTIVE
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_RIGHT_MID, 0, 0);
 #endif
 
 #if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
