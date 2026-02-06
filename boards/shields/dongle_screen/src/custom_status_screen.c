02ae0a637839c3148d96ca2fc7524d7d40f3172a
TooManyLs <toomanyls.2hi@gmail.com>
2026-02-06 10:03:58 +0700

    try layer roller from bwshockley's repo

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -14,6 +14,9 @@ static struct zmk_widget_output_status output_status_widget;
 #if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
 #include "widgets/layer_status.h"
 static struct zmk_widget_layer_status layer_status_widget;
+
+#include "widgets/layer_roller.h"
+static struct zmk_widget_layer_roller layer_roller_widget;
 #endif
 
 #if CONFIG_DONGLE_SCREEN_BATTERY_ACTIVE
@@ -68,7 +71,7 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_LEFT_MID, 10, 0);
 #endif
 
 #if CONFIG_DONGLE_SCREEN_MODIFIER_ACTIVE
