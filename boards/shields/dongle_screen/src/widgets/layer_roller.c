4117c3099e454d356c2bc2d09b540f2c37560f02
TooManyLs <toomanyls.2hi@gmail.com>
2026-02-07 02:38:03 +0700

    fiddle with text, turn off wpm meter

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -90,8 +90,10 @@ int zmk_widget_layer_roller_init(struct zmk_widget_layer_roller *widget, lv_obj_
     lv_obj_set_style_bg_opa(widget->obj, LV_OPA_TRANSP, LV_PART_SELECTED);
     lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_40, LV_PART_SELECTED);
     lv_obj_set_style_text_color(widget->obj, lv_color_hex(0xffffff), LV_PART_SELECTED);
-    lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_36, LV_PART_MAIN);
+    lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_32, LV_PART_MAIN);
     lv_obj_set_style_text_color(widget->obj, lv_color_hex(0x909090), LV_PART_MAIN);
+
+    lv_obj_set_style_pad_left(widget->obj, 10, LV_PART_MAIN); // move unselected label 10 pixels to the right
 
     int32_t fade_height = 40;
 
@@ -121,7 +123,7 @@ int zmk_widget_layer_roller_init(struct zmk_widget_layer_roller *widget, lv_obj_
     lv_obj_set_style_bg_grad_opa(fade_bottom, LV_OPA_COVER, 0);
     lv_obj_remove_flag(fade_bottom, LV_OBJ_FLAG_CLICKABLE | LV_OBJ_FLAG_SCROLLABLE);
 
-    lv_obj_set_style_anim_time(widget->obj, 50, 0);
+    lv_obj_set_style_anim_time(widget->obj, 100, 0);
 
     sys_slist_append(&widgets, &widget->node);
 
