9bd4cf069cbd4b0abcf4f4c40391105dd6883831
TooManyLs <toomanyls.2hi@gmail.com>
2026-02-08 22:39:46 +0700

    change roller colors

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -81,10 +81,10 @@ int zmk_widget_layer_roller_init(struct zmk_widget_layer_roller *widget, lv_obj_
     lv_obj_add_style(widget->obj, &style, 0);
     lv_obj_set_style_bg_opa(widget->obj, LV_OPA_TRANSP, LV_PART_SELECTED);
     lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_40, LV_PART_SELECTED);
-    lv_obj_set_style_text_color(widget->obj, lv_color_hex(0xffffff), LV_PART_SELECTED);
+    lv_obj_set_style_text_color(widget->obj, lv_color_hex(0x00a8ff), LV_PART_SELECTED);
     lv_obj_set_style_bg_opa(widget->obj, LV_OPA_TRANSP, LV_PART_MAIN);
     lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_34, LV_PART_MAIN);
-    lv_obj_set_style_text_color(widget->obj, lv_color_hex(0x909090), LV_PART_MAIN);
+    lv_obj_set_style_text_color(widget->obj, lv_color_hex(0xffffff), LV_PART_MAIN);
 
     lv_obj_set_style_anim_time(widget->obj, 200, 0);
 
