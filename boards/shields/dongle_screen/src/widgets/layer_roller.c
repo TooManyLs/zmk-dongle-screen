184ab0414028691fc6ced39452115a76b181c1cb
TooManyLs <toomanyls.2hi@gmail.com>
2026-02-08 22:03:28 +0700

    remove fade out effect on roller

--- boards/shields/dongle_screen/src/widgets/layer_roller.c
+++ boards/shields/dongle_screen/src/widgets/layer_roller.c
@@ -86,34 +86,6 @@ int zmk_widget_layer_roller_init(struct zmk_widget_layer_roller *widget, lv_obj_
     lv_obj_set_style_text_font(widget->obj, &lv_font_montserrat_34, LV_PART_MAIN);
     lv_obj_set_style_text_color(widget->obj, lv_color_hex(0x909090), LV_PART_MAIN);
 
-    int32_t fade_height = 40;
-
-    lv_obj_t *fade_top = lv_obj_create(widget->obj);
-    lv_obj_remove_style_all(fade_top);
-    lv_obj_set_size(fade_top, LV_PCT(100), fade_height);
-    lv_obj_align(fade_top, LV_ALIGN_TOP_MID, 0, 0);
-    lv_obj_set_style_bg_color(fade_top, lv_color_black(), 0);
-    lv_obj_set_style_bg_opa(fade_top, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_grad_color(fade_top, lv_color_black(), 0);
-    lv_obj_set_style_bg_grad_dir(fade_top, LV_GRAD_DIR_VER, 0);
-    lv_obj_set_style_opa(fade_top, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_main_opa(fade_top, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_grad_opa(fade_top, LV_OPA_TRANSP, 0);
-    lv_obj_remove_flag(fade_top, LV_OBJ_FLAG_CLICKABLE | LV_OBJ_FLAG_SCROLLABLE);
-
-    lv_obj_t *fade_bottom = lv_obj_create(widget->obj);
-    lv_obj_remove_style_all(fade_bottom);
-    lv_obj_set_size(fade_bottom, LV_PCT(100), fade_height);
-    lv_obj_align(fade_bottom, LV_ALIGN_BOTTOM_MID, 0, 0);
-    lv_obj_set_style_bg_color(fade_bottom, lv_color_black(), 0);
-    lv_obj_set_style_bg_opa(fade_bottom, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_grad_color(fade_bottom, lv_color_black(), 0);
-    lv_obj_set_style_bg_grad_dir(fade_bottom, LV_GRAD_DIR_VER, 0);
-    lv_obj_set_style_opa(fade_bottom, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_main_opa(fade_bottom, LV_OPA_TRANSP, 0);
-    lv_obj_set_style_bg_grad_opa(fade_bottom, LV_OPA_COVER, 0);
-    lv_obj_remove_flag(fade_bottom, LV_OBJ_FLAG_CLICKABLE | LV_OBJ_FLAG_SCROLLABLE);
-
     lv_obj_set_style_anim_time(widget->obj, 200, 0);
 
     sys_slist_append(&widgets, &widget->node);
